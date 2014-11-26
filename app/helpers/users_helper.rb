d395c99862b807533c64e2919635ce56be5bd4da
mumuli <alan_lin_2003@hotmail.com>
2014-11-26 00:41:11 -0800

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+	# Returns the Gravatar (http://gravatar.com/) for the given user.
+	def gravatar_for(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+		# Uncomment below for non-secure link
+		# gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}"
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt:user.name, class: "gravatar")
+	end
 end
